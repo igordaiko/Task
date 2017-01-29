f3b1ff3569e5fe48420dc1cf0eb8ee679e345e00
igordaiko <id_inworld@hotmail.com>
2017-01-29 14:59:57 +0200

    Task

---
+++ Task/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Task.Models;
+using System.Web.Mvc;
+namespace Task.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+
+        // GET api/values/5
+
+        public string Get(CulcModel value)
+        {
+            
+            return "value";
+        }
+
+        // POST api/values
+        public HttpResponseMessage Post(CulcModel value)
+        {
+            int result = 0;
+            if (value.operation == "+")
+                result = value.frs_num + value.scd_num;
+            else if (value.operation == "-")
+                result = value.frs_num - value.scd_num;
+            else if (value.operation == "*")
+                result = value.frs_num * value.scd_num;
+            else if (value.operation == "/")
+            {
+                if (value.scd_num != 0)
+                    result = value.frs_num / value.scd_num;
+                else
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "невозможно поделить на ноль");
+            }
+                
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Task/Models/CulcModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task.Models
+{
+    public class CulcModel
+    {
+        public int frs_num { get; set; }
+        public int scd_num { get; set; }
+        public string operation { get; set; }
+    }
+}
